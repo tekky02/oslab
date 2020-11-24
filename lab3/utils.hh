a7afdb8145f445d4709197389a99a04c6a7ae688
tekky02 <1657961253@qq.com>
2020-11-24 20:11:35 +0800

    modified

--- lab3/utils.hh
+++ lab3/utils.hh
@@ -5,8 +5,8 @@
  * @Last Modified time: 2020-11-11 18:37:59
  */
 
-#ifndef __UTILS_H__
-#define __TUILS_H__
+#ifndef __UTILS_HH__
+#define __TUILS_HH__
 
 // P operation.
 void P(int semid, int index);
@@ -14,4 +14,4 @@ void P(int semid, int index);
 // V operation.
 void V(int semid, int index);
 
-#endif /* __UTILS_H__ */
+#endif /* __UTILS_HH__ */
